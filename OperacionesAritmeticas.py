16a462b242d285a9b118f36f20fe06fa8f3776c1
lexus210 <73870803@continental.edu.pe>
2024-09-02 18:52:17 -0500

    Proyecto de suma en POO

---
+++ OperacionesAritmeticas.py
@@ -0,0 +1,6 @@
+from OperacionesAritmeticas import OperacionesAritmeticas
+
+if __name__=='__main__':
+    operacion = OperacionesAritmeticas()
+    num1, num2 = operacion.ingresoNumeros()
+    print(f"{num1} + {num2} = {operacion.suma(num1, num2)}")
