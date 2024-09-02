d422e5ee43395520ccdcde324f7ecb0d640b2d52
lexus210 <73870803@continental.edu.pe>
2024-09-02 18:38:00 -0500

    Proyecto de suma V1_1

--- Suma_V1_0.py
+++ Suma_V1_0.py
@@ -1,4 +1,4 @@
-numero1=float(input("Ingrese sumando uno: "))
-numero2=float(input("Ingrese sumando dos: "))
-suma=numero1+numero2
+numero1 = float(input("Ingrese sumando uno: "))
+numero2 = float(input("Ingrese sumando dos: "))
+suma = numero1 + numero2
 print(f"{numero1} + {numero2} = {suma}")
